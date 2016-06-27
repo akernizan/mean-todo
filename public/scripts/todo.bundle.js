c12f45cf26f58d93d040c091932b048e93935afc
akosua kernizan <akernizan@gmail.com>
2016-06-27 16:33:38 -0400

    added webpack, angular

---
+++ public/scripts/todo.bundle.js
@@ -0,0 +1,14 @@
+webpackJsonp([0],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var angular = __webpack_require__(1);
+
+	angular.module('todoListApp', []);
+
+	__webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"scripts/controllers/main.js\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()))
+
+/***/ }
+]);
---
+++ src/api/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var express = require('express');
+var todos = require('../../mock/todos.json');
+
+var router = express.Router();
+
+router.get('/todos', function (req, res) {
+	res.json({
+		todos: todos
+	});
+});
+
+//TODO: Add POST route to create new entries
+
+
+//TODO: Add PUT route to update exsisting entries
+
+
+//TODO: Add DELETE route to delete exsisting entries
+
+
+module.exports = router;
